cd4ab4aa7830e8c82ff5a847f6767e0129b0b47d
aryans41 <aryans41@nycstudents.net>
2025-01-07 00:46:51 +0000

    m

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -1,12 +1,12 @@
 package com.example.project;
 public class Main {
-    public static void main(String[] args) {
-        Fibonacci fib3 = new Fibonacci(5);
-        System.out.println(fib3.fibonacciString());
-        
-        int[] seq3 = fib3.getSequence();
-        seq3[0] = 100;
-        
-        System.out.println(fib3.fibonacciString());        
+    public static void main(String[] args) { 
+        Fibonacci fib0 = new Fibonacci (5); 
+        Fibonacci fib1 = new Fibonacci (11);  
+        Fibonacci fib2 = new Fibonacci (20); 
+        Fibonacci [] fibonaccis = {fib0, fib1, fib2};
+        for (Fibonacci i : fibonaccis) {
+            System.out.println(i.getSequence().length);
+        }
     }
 }
